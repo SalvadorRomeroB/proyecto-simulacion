f63656dac1ba66e4dd0e105548b4eb02039586a3
Salva Romero <chava0romero@Salvas-MacBook-Pro.local>
2020-04-17 13:37:26 -0600

    missing 4

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TypeSelector from "./typeSelector";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <TypeSelector />
+      <h1>Generador </h1>
+    </div>
+  );
+};
+
+export default Header;
