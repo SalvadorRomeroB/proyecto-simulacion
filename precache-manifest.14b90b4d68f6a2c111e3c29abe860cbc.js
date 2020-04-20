cb7047b4d9112c65dfd6f6898a606af4487b3020
Salva Romero <chava0romero@Salvas-MacBook-Pro.local>
2020-04-19 22:14:05 -0600

    Updates

---
+++ precache-manifest.14b90b4d68f6a2c111e3c29abe860cbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72950ab92c24906459e63f5768d0eedd",
+    "url": "/Proyecto-Simulacion/index.html"
+  },
+  {
+    "revision": "812d2920e7620667c9a7",
+    "url": "/Proyecto-Simulacion/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "13b4901b256fad4ca568",
+    "url": "/Proyecto-Simulacion/static/css/main.b6bf3f91.chunk.css"
+  },
+  {
+    "revision": "812d2920e7620667c9a7",
+    "url": "/Proyecto-Simulacion/static/js/2.83f8af50.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Proyecto-Simulacion/static/js/2.83f8af50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13b4901b256fad4ca568",
+    "url": "/Proyecto-Simulacion/static/js/main.bc39ec7a.chunk.js"
+  },
+  {
+    "revision": "45baaf289b21532e4c8c",
+    "url": "/Proyecto-Simulacion/static/js/runtime-main.ad84d5b1.js"
+  }
+]);
